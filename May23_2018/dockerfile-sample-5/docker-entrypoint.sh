7e9ec7cc44946434ba159d312b825c45b43dcd77
Surendra Bajracharya <bajracharya.surendra@gmail.com>
2018-05-21 22:19:25 -0500

    testing

--- May23_2018/dockerfile-sample-5/docker-entrypoint.sh
+++ May23_2018/dockerfile-sample-5/docker-entrypoint.sh
@@ -5,3 +5,9 @@ cat /etc/hosts
 echo "ok i'm done, now let's ping'"
 
 exec "$@"
+
+# set -e - exit script if any command fails (non-zero value)
+# exec "$@" is typically used to make the entrypoint a pass through that then runs the docker command.
+#
+
+
